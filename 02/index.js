efae9dde35a6988a9d4627824eb5a77d2857a821
Tamires Cavalcante <tmires.cavalcante@gmail.com>
2022-03-16 15:18:28 -0300

    learning docker-compose

---
+++ 02/index.js
@@ -0,0 +1,16 @@
+import './setup/db'
+
+import { server } from './setup/server'
+import { UserModel } from './models/user'
+
+server.get('/users', async (req, res) => {
+  try {
+    const users = await UserModel.find()
+    res.send(users)
+  } catch (error) {
+    res.send(500, error)
+  }
+})
+
+
+server.start(() => console.log('Started server'))
